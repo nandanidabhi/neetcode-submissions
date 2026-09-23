fd5100995ee3048e0b05e276e7eac8f4bac2ba59
nandanidabhi <46027741+nandanidabhi@users.noreply.github.com>
2026-09-23 11:04:27 -0700

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        if(intervals == null || intervals.length <=1){
+            return intervals;
+        }
+        //Sorted Intervals
+        final int[][] sortedIntervals = intervals;
+        Arrays.sort(sortedIntervals,(a,b) -> Integer.compare(a[0],b[0]));
+
+        final List<int[]> mergedSegments = new ArrayList<>();
+
+        //Initialize tracking with first sorted array
+        int[] currentInterval = sortedIntervals[0];
+        mergedSegments.add(currentInterval);
+        
+
+        for(int i =1;i<sortedIntervals.length;i++){
+
+            final int nextStart = sortedIntervals[i][0];
+            final int nextEnd = sortedIntervals[i][1];
+
+            if(nextStart <= currentInterval[1]){
+                currentInterval[1] = Math.max(nextEnd, currentInterval[1]);
+            } else {
+                currentInterval=sortedIntervals[i];
+                mergedSegments.add(currentInterval);
+            }
+
+
+        }
+
+        return mergedSegments.toArray(new int[mergedSegments.size()][]);
+    }
+}
